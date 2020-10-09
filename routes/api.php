f4de62c271f7f9e4e4b12232a50024010704036f
agreerga <agreerga@hotmail.com>
2020-10-09 12:23:15 +0800

    fix api name

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/nmw/debug', 'api\TestController@test');
-Route::get('/nmw/cgssids', 'api\CGSSController@debug');
+Route::get('/nmw/cgssids', 'api\CGSSController@test');
 
